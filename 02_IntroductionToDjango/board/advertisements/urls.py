42a5330f7a8d867448d1952d292ce1e2142783fb
latyshev_egor <egorlat5g@mail.ru>
2021-08-13 18:12:42 +0300

    homework finished

--- 02_IntroductionToDjango/board/advertisements/urls.py
+++ 02_IntroductionToDjango/board/advertisements/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.advertisements_list, name='advertisements'),
-    path("advertisement", views.advertisement, name='advertisement_detailed')
+    path("", views.home, name='i_sell_homework'),
+    path("python-basic", views.python_basic, name='python-basic'),
+    path("django", views.django_framework, name='django-framework'),
+    path("os-linux", views.os_linux, name='os-linux'),
+    path("sql", views.sql, name='sql'),
+    path("web-layout", views.web_layout, name='web-layout')
+
 ]
--- 02_IntroductionToDjango/board/advertisements/views.py
+++ 02_IntroductionToDjango/board/advertisements/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def home(request):
+    return render(request, 'advertisements/i_sell_homework.html')
+
+
 def advertisements_list(request):
     return render(request, 'advertisements/advertisements_list.html')
 
 
 def advertisement(request):
     return render(request, 'advertisements/advertisement.html')
+
+
+def python_basic(request):
+    return render(request, 'advertisements/python-basic.html')
+
+
+def django_framework(request):
+    return render(request, 'advertisements/django.html')
+
+
+def os_linux(request):
+    return render(request, 'advertisements/os-linux.html')
+
+
+def sql(request):
+    return render(request, 'advertisements/sql.html')
+
+
+def web_layout(request):
+    return render(request, 'advertisements/web-layout.html')
+
